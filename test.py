f175e76981d2e56ba29862376760800ed7dd48bd
Ashok Nune <nunezashok@gmail.com>
2025-05-29 16:35:11 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,24 @@
+import pyodbc
+
+try:
+    conn = pyodbc.connect(
+        'DRIVER={SQL Server};'
+        'SERVER=localhost;'
+        'DATABASE=YourDatabaseName;'
+        'UID=YourUsername;'
+        'PWD=YourPassword;'
+    )
+    cursor = conn.cursor()
+    # Example data to insert
+    data_to_insert = [
+        (1,'your_name', 'your_email_ID')
+    ]
+    # Insert data into the table
+    cursor.executemany(
+        "INSERT INTO testTable (id,email,name ) VALUES (?,?, ?)",
+        data_to_insert
+    )
+    conn.commit()
+    print("Data inserted successfully.")
+except pyodbc.Error as e:
+    print("Error while connecting to SQL Server:", e)   
